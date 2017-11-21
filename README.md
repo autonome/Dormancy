8cbccfc39939cc188d46e590b3c8488aed9426ee
dietrich ayala <autonome@gmail.com>
2017-11-21 12:03:34 +0700

    port to WebExtensions API

--- README.md
+++ README.md
@@ -1,17 +1,7 @@
 Dormancy 'retires' tabs that have gone unused for a while, freeing up that memory. It then restores the tabs to life when accessed.
 
-While Firefox 9 adds restore-on-demand for users that restore their session by default, many users will never benefit from it. This add-on targets users who don't restore session, but do have long-running instances of Firefox and many tabs. This might land as a core feature in Firefox 9 (https://bugzilla.mozilla.org/show_bug.cgi?id=675539).
+Tabs are considered inactive when they haven't been selected in longer than 5 minutes.
 
-NOTE: This is highly experimental, has only been tested on the Nightly builds, and probably will destroy your session. You've been warned.
+Tabs are checked for inactivity every 5 minutes.
 
-Tabs are considered inactive when they haven't been selected in longer than 5 minutes. To change that, set this pref to a value in milliseconds:
-
-* extensions.dormancy.TabDormancyAgeMs
-
-Tabs are checked for inactivity every 5 minutes. To change this, set this pref to a value in milliseconds:
-
-* extensions.dormancy.TabCheckIntervalMs
-
-EXPERIMENTAL. MAY EAT YOUR SESSION OR DO OTHER BAD THINGS.
-
-<a href='http://j.mp/oW3xQN'>INSTALL</a>
+Install: https://addons.mozilla.org/en-US/firefox/addon/dormancy/
---
+++ background.js
@@ -0,0 +1,55 @@
+// Age at which tabs should be dormanticized (default 5 mins)
+//const TAB_AGE_MS = 5 * 60 * 1000;
+const TAB_AGE_MS = 15 * 1000;
+
+// Storage key
+const TAB_LAST_ACTIVE_KEY = 'DORMANCY_TAB_LAST_ACTIVE';
+
+// Store the current timestamp on a tab
+async function setTabLastActive(tabId) {
+  browser.sessions.setTabValue(tabId, TAB_LAST_ACTIVE_KEY, Date.now());
+}
+
+// When a tab is selected, store current timestamp
+browser.tabs.onActivated.addListener(info => {
+  setTabLastActive(info.tabId);
+});
+
+// bool whether or not tab is old enough to make dormant
+async function tabIsOld(tabId) {
+  // Get tab last active date
+  const lastActive = await browser.sessions.getTabValue(tabId, TAB_LAST_ACTIVE_KEY);
+
+  // Not yet stored
+  // (will happen next time tab is made active)
+  if (!lastActive) {
+    return false;
+  }
+
+  // If last active time is past the dormancy trigger
+  if (Date.now() - lastActive >= TAB_AGE_MS) {
+    return true;
+  }
+
+  return false;
+}
+
+// Check for tabs that have hit dormanticizable age and dormanticize them.
+async function periodicTabCheck() {
+  // Query for the active tab
+  let activeTabs = await browser.tabs.query({ active: true, currentWindow: true })
+  let activeTabId = activeTabs[0].id;
+
+  // Query for all tabs that are not the active tab
+  let tabs = await browser.tabs.query({});
+
+  for (let i in tabs) {
+    let tab = tabs[i];
+    let isOld = await tabIsOld(tab.id);
+    if (tab.id != activeTabId && !tab.discarded && isOld) {
+      browser.tabs.discard(tab.id);
+    }
+  }
+}
+
+setInterval(periodicTabCheck, TAB_AGE_MS);
